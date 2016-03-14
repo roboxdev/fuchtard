2408e77ba4f92cd91753c771aa1dc2c4e98e4ce1
robox <roboxv@gmail.com>
2016-03-14 12:35:18 +0600

    index page replaced with food-menu

--- fuchtard/food/urls.py
+++ fuchtard/food/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from .views import FoodMenuView
 
 urlpatterns = [
-    url(r'^menu/', FoodMenuView.as_view(), name='food-menu-view'),
-    # url(r'^menu/465', FoodMenuView.as_view(), name='food-menu-view-food-item'),
+    url(r'^$', FoodMenuView.as_view(), name='food-menu-view'),
 ]
 
--- fuchtard/main/urls.py
+++ fuchtard/main/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
-from .views import IndexView
-
 urlpatterns = [
-    url(r'^$', IndexView.as_view(template_name='main/index.html'), name='index-view'),
     url(r'^contacts/$', TemplateView.as_view(template_name='main/contacts.html'), name='contacts-view'),
     url(r'^eula/$', TemplateView.as_view(template_name='main/eula.html'), name='eula-view'),
     url(r'^faq/$', TemplateView.as_view(template_name='main/faq.html'), name='faq-view'),
