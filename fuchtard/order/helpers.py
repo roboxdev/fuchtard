fd12ba6119b0bc225058533b4c7b5f75e0a0ede1
robox <roboxv@gmail.com>
2016-05-25 19:19:20 +0700

    send_templated_email wrapper function added

--- fuchtard/order/helpers.py
+++ fuchtard/order/helpers.py
@@ -1,3 +1,20 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+
 def shifthash(n):
     n = int(n)
     return ((0x000FFF & n) << 12) + ((0xFFF000 & n) >> 12)
+
+
+def send_templated_email(params):
+    template = params.get('template')
+    msg_plain = render_to_string('{}.txt'.format(template), params.get('template_params'))
+    msg_html = render_to_string('{}.html'.format(template), params.get('template_params'))
+    send_mail(
+        subject=params.get('subject'),
+        message=msg_plain,
+        from_email=params.get('from_email'),
+        recipient_list=params.get('recipient_list'),
+        html_message=msg_html,
+    )
