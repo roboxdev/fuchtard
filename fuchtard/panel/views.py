ed6391135a100945955438ed530f768757827194
robox <roboxv@gmail.com>
2016-05-26 18:39:42 +0700

    access to panel is restricted now

--- fuchtard/panel/views.py
+++ fuchtard/panel/views.py
@@ -1,19 +1,28 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView, ListView, DetailView
 
 from order.models import Order
 from order.helpers import shifthash
 
 
-class DashboardView(TemplateView):
+class CanEditOrderPermissionRequiredMixin(PermissionRequiredMixin):
+    permission_required = 'order.can_edit'
+
+    def get_login_url(self):
+        return reverse('admin:login')
+
+
+class DashboardView(CanEditOrderPermissionRequiredMixin, TemplateView):
     template_name = 'panel/dashboard.html'
 
 
-class OrdersView(ListView):
+class OrdersView(CanEditOrderPermissionRequiredMixin, ListView):
     template_name = 'panel/orders.html'
     model = Order
 
 
-class OrderDetailView(DetailView):
+class OrderDetailView(CanEditOrderPermissionRequiredMixin, DetailView):
     template_name = 'panel/order_detail.html'
     model = Order
 
