f3316e19d8592051717e7d2530b466247733a009
robox <roboxv@gmail.com>
2017-01-08 01:54:47 +0600

    set serializers field explicitly

--- src/fuchtard/food/serializers.py
+++ src/fuchtard/food/serializers.py
@@ -3,8 +3,26 @@ from .models import FoodCategory, FoodItem
 
 
 class FoodItemSerializer(serializers.ModelSerializer):
+    price = serializers.SerializerMethodField()
+
     class Meta:
         model = FoodItem
+        fields = (
+            'id',
+            'visible',
+            'enabled',
+            'position',
+            'title',
+            'photo',
+            'description',
+            'raw_price',
+            'category',
+            'tags',
+            'price',
+        )
+
+    def get_price(self, obj):
+        return obj.price
 
 
 class FoodCategorySerializer(serializers.ModelSerializer):
