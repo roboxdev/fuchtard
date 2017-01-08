0f7b27a7a62b835e86279eb0f51666fe51c6c60f
robox <roboxv@gmail.com>
2017-01-09 00:32:52 +0600

    pep8

--- src/fuchtard/order/urls.py
+++ src/fuchtard/order/urls.py
@@ -6,5 +6,7 @@ from .views import OrderCheckoutView, CartUpdateView
 urlpatterns = [
     url(r'^order/$', OrderCheckoutView.as_view(), name='order-checkout-view'),
     url(r'^cart/update/$', CartUpdateView.as_view(), name='cart-update-view'),
-    url(r'^thanks/(?P<hashed_id>[0-9]+)/$', TemplateView.as_view(template_name='order/thank_you.html'), name='thank-you-view'),
+    url(r'^thanks/(?P<hashed_id>[0-9]+)/$',
+        TemplateView.as_view(template_name='order/thank_you.html'),
+        name='thank-you-view'),
 ]
