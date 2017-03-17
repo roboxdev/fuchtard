ad3661ccb1cde1b4e5fb161fd01ea3a6d9bc3db6
robox <roboxv@gmail.com>
2017-03-17 21:53:12 +0700

    taking csrf token from template rather than from cookies

--- nodejs/src/actions/app.js
+++ nodejs/src/actions/app.js
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-fetch';
-import * as Cookies from 'js-cookie';
 
-const csrftoken = Cookies.get('csrftoken');
+const csrftoken = window.config.csrfToken;
 
 
 function cartItemIncrease(foodItemId) {
