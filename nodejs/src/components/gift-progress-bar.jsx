80105380d1074f28b4c5c6cb4254c0ba1284f9f5
robox <roboxv@gmail.com>
2017-03-23 20:56:45 +0700

    new feature: progressbar for gifts. layout fixes needed

---
+++ nodejs/src/components/gift-progress-bar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ProgressBar from 'react-toolbox/lib/progress_bar';
+
+import {subtotalSelector} from 'selectors/app';
+
+
+@connect(
+    state => ({
+        cartPrice: subtotalSelector(state),
+        gifts: state.gifts,
+    })
+)
+export class GiftProgressBar extends React.Component {
+    getMaxProgress = () => [...this.props.gifts].pop().requirement;
+
+    getBuffer = () => {
+        const buffer = this.props.gifts.filter(v => v.requirement > this.props.cartPrice)[0];
+        return buffer ? buffer.requirement : this.getMaxProgress();
+    };
+
+    render() {
+        const {cartPrice, gifts} = this.props;
+        return (cartPrice && gifts
+            ? <ProgressBar mode='determinate'
+                           value={cartPrice}
+                           buffer={this.getBuffer()}
+                           max={this.getMaxProgress()}
+            />
+            : null)
+
+    }
+}
--- nodejs/src/components/navbar.jsx
+++ nodejs/src/components/navbar.jsx
@@ -5,37 +5,39 @@ import FontIcon from 'react-toolbox/lib/font_icon';
 import styles from '../styles/navbar.styl';
 import buttonStyles from '../styles/navbar-button.styl';
 
+import {GiftProgressBar} from 'components/gift-progress-bar';
+
+class NavigationButtons extends React.Component {
+    render() {
+        return <div styleName="styles.navigation-buttons">
+            <Link to="/" styleName="styles.link">
+                <Button neutral={false} theme={buttonStyles}>
+                    <FontIcon styleName="styles.button-icon" value="home"/>
+                    <span styleName="styles.button-text">Домой</span>
+                </Button>
+            </Link>
+            <Link to="/cart" styleName="styles.link">
+                <Button neutral={false} theme={buttonStyles}>
+                    <FontIcon styleName="styles.button-icon" value="shopping_cart"/>
+                    <span styleName="styles.button-text">Корзина</span>
+                </Button>
+            </Link>
+            <Link to="/checkout" styleName="styles.link">
+                <Button neutral={false} theme={buttonStyles}>
+                    <FontIcon styleName="styles.button-icon" value="credit_card"/>
+                    <span styleName="styles.button-text">Оформить</span>
+                </Button>
+            </Link>
+        </div>
+
+    }
+}
 
 export class NavBar extends React.Component {
-    state = {
-        selectedIndex: 0,
-    };
-
-    select = (index) => this.setState({selectedIndex: index});
-
     render() {
-        return (
-
-            <div styleName="styles.sticky">
-                <Link to="/" styleName="styles.link">
-                    <Button neutral={false} theme={buttonStyles}>
-                        <FontIcon styleName="styles.button-icon" value="home"/>
-                        <span styleName="styles.button-text">Домой</span>
-                    </Button>
-                </Link>
-                <Link to="/cart" styleName="styles.link">
-                    <Button neutral={false} theme={buttonStyles}>
-                        <FontIcon styleName="styles.button-icon" value="shopping_cart"/>
-                        <span styleName="styles.button-text">Корзина</span>
-                    </Button>
-                </Link>
-                <Link to="/checkout" styleName="styles.link">
-                    <Button neutral={false} theme={buttonStyles}>
-                        <FontIcon styleName="styles.button-icon" value="credit_card"/>
-                        <span styleName="styles.button-text">Оформить</span>
-                    </Button>
-                </Link>
-            </div>
-        )
+        return <div styleName="styles.sticky">
+            <GiftProgressBar />
+            <NavigationButtons />
+        </div>
     }
 }
