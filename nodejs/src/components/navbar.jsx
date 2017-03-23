e30f6bd59cbc66c4a5cb3382244fbabc4b871742
Telarn <telarn@gmail.com>
2017-03-23 18:36:16 +0700

    added icons, styles. initial navbar layout

--- nodejs/src/components/navbar.jsx
+++ nodejs/src/components/navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Button} from 'react-toolbox/lib/button';
+import FontIcon from 'react-toolbox/lib/font_icon';
 import styles from '../styles/navbar.styl';
+import buttonStyles from '../styles/navbar-button.styl';
 
 
 export class NavBar extends React.Component {
@@ -12,10 +15,26 @@ export class NavBar extends React.Component {
 
     render() {
         return (
-            <div styleName="sticky">
-                <Link to="/"><i className="material-icons">build</i></Link>
-                <Link to="/cart">Корзина</Link>
-                <Link to="/checkout">Оформить</Link>
+
+            <div styleName="styles.sticky">
+                <Link to="/" styleName="styles.link">
+                    <Button neutral={false} theme={buttonStyles}>
+                        <FontIcon styleName="styles.button-icon" value="home"/>
+                        <span styleName="styles.button-text">Домой</span>
+                    </Button>
+                </Link>
+                <Link to="/cart" styleName="styles.link">
+                    <Button neutral={false} theme={buttonStyles}>
+                        <FontIcon styleName="styles.button-icon" value="shopping_cart"/>
+                        <span styleName="styles.button-text">Корзина</span>
+                    </Button>
+                </Link>
+                <Link to="/checkout" styleName="styles.link">
+                    <Button neutral={false} theme={buttonStyles}>
+                        <FontIcon styleName="styles.button-icon" value="credit_card"/>
+                        <span styleName="styles.button-text">Оформить</span>
+                    </Button>
+                </Link>
             </div>
         )
     }
