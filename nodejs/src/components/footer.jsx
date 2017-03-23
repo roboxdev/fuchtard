b8ead371507fc3ffbe7c59ea0f680e11b2bee180
Telarn <telarn@gmail.com>
2017-03-23 18:43:11 +0700

    .gitignored .floo Added bottom padding so that the navbar does not overlaps footer

--- nodejs/src/components/footer.jsx
+++ nodejs/src/components/footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../styles/footer.styl';
 
 export class Footer extends React.Component {
     render() {
         return (
-            <footer>
+            <footer styleName="styles.footer">
                 <div>
                     <div>
                         <div>
