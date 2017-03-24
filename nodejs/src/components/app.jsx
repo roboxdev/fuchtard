2fcd701a6268a141b5b12dec4815cefe02dd367a
robox <roboxv@gmail.com>
2017-03-25 02:09:46 +0700

    boilerplate for different layout on desktop

--- nodejs/src/components/app.jsx
+++ nodejs/src/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-
+import {FoodMenu} from 'components/menu';
 import {IndexPage} from 'components/index-page';
 import {Checkout} from 'components/checkout';
 import {NavBar} from 'components/navbar';
@@ -16,9 +16,11 @@ export class App extends React.Component {
             <Router>
                 <div styleName="styles.app">
                     <div styleName="styles.content">
+                        <div styleName="styles.food-menu-container"><FoodMenu/></div>
                         <Route exact path="/" component={IndexPage}/>
                         <Route path="/cart/" component={Checkout}/>
                         <Route path="/:slug/" component={FoodCategory}/>
+                        <div styleName="styles.checkout-container"><Checkout/></div>
                     </div>
                     <NavBar />
                 </div>
