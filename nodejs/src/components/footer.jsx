b16660757e79435fb96e978eaf35d762b073877c
robox <roboxv@gmail.com>
2017-03-25 01:22:29 +0700

    obsolete urls removed

--- nodejs/src/components/footer.jsx
+++ nodejs/src/components/footer.jsx
@@ -5,30 +5,9 @@ export class Footer extends React.Component {
     render() {
         return (
             <footer styleName="styles.footer">
-                <div>
-                    <div>
-                        <div>
-                            <p>
-                                <a href="{% url 'main:contacts-view' %}">О компании и контакты</a>
-                            </p>
-                            <p>
-                                <a href="{% url 'main:faq-view' %}">Вопросы и ответы</a>
-                            </p>
-                            <p>
-                                <a href="{% url 'main:eula-view' %}">Пользовательское соглашение</a>
-                            </p>
-                        </div>
-                        <div>
-                            <p>TОО «Макси суши»</p>
-                            <p>Телефон: <a href="tel:+77273573030">+7 (727) 357 30 30</a></p>
-                            <p>Наша почта: <a href="mailto:ok@maxisushi.kz">ok@maxisushi.kz</a></p>
-                        </div>
-                        <div>
-                            <p><a href="{% url 'panel:dashboard-view' %}">Войти</a></p>
-                            <p>Разработка: <a href="https://roboxv.pro">robox</a></p>
-                        </div>
-                    </div>
-                </div>
+                <p>TОО «Макси суши»</p>
+                <p>Телефон: <a href="tel:+77273573030">+7 (727) 357 30 30</a></p>
+                <p>Наша почта: <a href="mailto:ok@maxisushi.kz">ok@maxisushi.kz</a></p>
             </footer>
         )
     }
