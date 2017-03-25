ada05d2a9e87d31b8af70fe311dd620e1553b7c8
robox <roboxv@gmail.com>
2017-03-26 03:32:14 +0700

    order form refactored order form collapsed until phone field focused

--- nodejs/src/components/order.jsx
+++ nodejs/src/components/order.jsx
@@ -8,6 +8,7 @@ import {Button} from 'react-toolbox/lib/button';
 import styles from '../styles/checkout.css';
 import buttonStyles from '../styles/order-button.css';
 import inputStyles from '../styles/order-input.css';
+import Collapse from 'react-collapse';
 
 
 @connect(
@@ -20,56 +21,76 @@ import inputStyles from '../styles/order-input.css';
     })
 )
 export class OrderForm extends React.Component {
+    state = {
+        optionalFieldsVisible: false,
+    };
+
+    showOptionalFields = () => this.setState({optionalFieldsVisible: true});
+
+    submit = (e) => {
+        e.preventDefault();
+        this.props.placeOrder();
+    }
+
     render() {
         const {order, updateOrderField} = this.props;
         const {address} = order;
         const updateOrder = (field, value) => updateOrderField(field, value, false);
         const updateAddress = (field, value) => updateOrderField(field, value, true);
         return <div styleName="styles.wrapper">
-            Order Form
-            <Input theme={inputStyles}
-                   value={order.name}
-                   onChange={value => updateOrder('name', value)}
-                   type='text'
-                   name='name'
-                   label='Имя'
-                   maxLength={16}
-            />
-            <Input theme={inputStyles}
-                   value={order.phone}
-                   onChange={value => updateOrder('phone', value)}
-                   type='tel'
-                   name='phone'
-                   label='Телефон'
-            />
-            <Input theme={inputStyles}
-                   value={address.street}
-                   onChange={value => updateAddress('street', value)}
-                   type='text'
-                   name='street'
-                   label='Улица'
-            />
-            <Input theme={inputStyles}
-                   value={address.building}
-                   onChange={value => updateAddress('building', value)}
-                   type='text'
-                   name='building'
-                   label='Номер дома'
-            />
-            <Input theme={inputStyles}
-                   value={address.apartment}
-                   onChange={value => updateAddress('apartment', value)}
-                   type='text'
-                   name='apartment'
-                   label='Квартира'
-            />
-            <Input theme={inputStyles}
-                   value={order.comment}
-                   onChange={value => updateOrder('comment', value)}
-                   type='text'
-                   label='Комментарий'
-            />
-            <Button theme={buttonStyles} onClick={this.props.placeOrder} raised primary>Заказать</Button>
+            <form onSubmit={this.submit}>
+                <Input theme={inputStyles}
+                       value={order.name}
+                       onChange={value => updateOrder('name', value)}
+                       type='text'
+                       name='name'
+                       label='Имя'
+                       maxLength={16}
+                       required
+                />
+                <Input theme={inputStyles}
+                       value={order.phone}
+                       onChange={value => updateOrder('phone', value)}
+                       onFocus={this.showOptionalFields}
+                       type='tel'
+                       name='phone'
+                       label='Телефон'
+                       required
+                />
+                <Collapse isOpened={this.state.optionalFieldsVisible}>
+                    <div>
+                        <p>Следующие поля необязательны. Мы уточним детали по телефону.</p>
+                        <Input theme={inputStyles}
+                               value={address.street}
+                               onChange={value => updateAddress('street', value)}
+                               type='text'
+                               name='street'
+                               label='Улица'
+                        />
+                        <Input theme={inputStyles}
+                               value={address.building}
+                               onChange={value => updateAddress('building', value)}
+                               type='text'
+                               name='building'
+                               label='Номер дома'
+                        />
+                        <Input theme={inputStyles}
+                               value={address.apartment}
+                               onChange={value => updateAddress('apartment', value)}
+                               type='text'
+                               name='apartment'
+                               label='Квартира'
+                        />
+                        <Input theme={inputStyles}
+                               value={order.comment}
+                               onChange={value => updateOrder('comment', value)}
+                               type='text'
+                               label='Комментарий'
+                        />
+                    </div>
+                </Collapse>
+                <Button theme={buttonStyles} type="submit" raised primary>Заказать</Button>
+            </form>
         </div>
     }
 }
