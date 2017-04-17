41ad1621cf849fc0f54a0359da4a586e5924547c
robox <roboxv@gmail.com>
2017-04-17 12:04:36 +0600

    extracting global styles into separate file

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,5 +4,6 @@ import {Provider} from 'react-redux';
 import {store} from 'store';
 import App from 'components/App';
 
+import 'styles/_globals.css';
 
 render(<Provider store={store}><App /></Provider>, document.getElementById('js-react-app'));
