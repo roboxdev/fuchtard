e8bfe847d2826d693d70a1cd504353f55835f609
robox <roboxv@gmail.com>
2017-04-21 19:40:34 +0600

    WIP: header layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,8 @@ import {Dialog} from 'react-toolbox/lib/dialog';
 
 import CheckoutHowto from 'components/CheckoutHowto';
 
+import styles from 'styles/Header.css';
+
 export class Header extends React.Component {
     state = {
         howtoVisible: false,
@@ -16,50 +18,55 @@ export class Header extends React.Component {
     render() {
         return (
             <header>
-                <div>
-                    <div>
+                <div className={styles.headerWrapper}>
+                    <div className={styles.logo}>
                         <Link to="/">
                             <img src="/public/maxiSushiLogo.png"/>
                         </Link>
                     </div>
-                    <div>
-                        <p>
-                            <span>Телефон для заказов:</span>
-                            <span><a href="tel:+77273573030">+7 (727) 357 30 30</a></span>
-                        </p>
-                        <p>
-                            <span>Мобильный:</span>
-                            <span><a href="tel:+77757210135">+7 (775) 721 01 35</a></span>
-                        </p>
-                        <p>
-                            <span>Мы в сети:</span>
-                            <a target="_blank" href="https://www.facebook.com/Доставка-суши-MaxiSushi-568292479939728/">
-                                <img src="/public/facebook-box.svg"/>
-                            </a>
-                            <a target="_blank" href="https://instagram.com/maxi.sushi/">
-                                <img src="/public/instagram.svg"/>
-                            </a>
-                            <a target="_blank" href="https://vk.com/club79840974">
-                                <img src="/public/vk-box.svg"/>
-                            </a>
-                        </p>
+                    <div className={styles.aboutUsWrapper}>
+                        <div className={styles.aboutUs}>
+                            <p>
+                                <span>Телефон для заказов:</span>
+                                <span><a href="tel:+77273573030">+7 (727) 357 30 30</a></span>
+                            </p>
+                            <p>
+                                <span>Мобильный:</span>
+                                <span><a href="tel:+77757210135">+7 (775) 721 01 35</a></span>
+                            </p>
+                            <p>
+                                <span>Мы в сети:</span>
+                                <a target="_blank"
+                                   href="https://www.facebook.com/Доставка-суши-MaxiSushi-568292479939728/">
+                                    <img src="/public/facebook-box.svg"/>
+                                </a>
+                                <a target="_blank" href="https://instagram.com/maxi.sushi/">
+                                    <img src="/public/instagram.svg"/>
+                                </a>
+                                <a target="_blank" href="https://vk.com/club79840974">
+                                    <img src="/public/vk-box.svg"/>
+                                </a>
+                            </p>
+                            <div className={styles.howtoButtonWrapper}>
+                                <IconButton
+                                    primary={true}
+                                    icon={'help'}
+                                    onClick={this.howtoToggle}
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <IconButton
-                        primary={true}
-                        icon={'help'}
-                        onClick={this.howtoToggle}
-                    />
-                    <Dialog
-                        actions={[{ label: "Закрыть", onClick: this.howtoToggle }]}
-                        active={this.state.howtoVisible}
-                        onEscKeyDown={this.howtoToggle}
-                        onOverlayClick={this.howtoToggle}
-                        title='Справка'
-                    >
-                        <CheckoutHowto/>
-                    </Dialog>
 
                 </div>
+                <Dialog
+                    actions={[{ label: "Закрыть", onClick: this.howtoToggle }]}
+                    active={this.state.howtoVisible}
+                    onEscKeyDown={this.howtoToggle}
+                    onOverlayClick={this.howtoToggle}
+                    title='Справка'
+                >
+                    <CheckoutHowto/>
+                </Dialog>
             </header>
         )
     }
