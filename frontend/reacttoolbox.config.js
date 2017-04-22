2ee025d1449fd88551202070a2468347f09a892a
robox <roboxv@gmail.com>
2017-04-22 11:27:55 +0600

    this way we can use var(--primary-color) by importing '_constants'

--- frontend/reacttoolbox.config.js
+++ frontend/reacttoolbox.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     variables: {
         'color-primary': 'var(--palette-deep-orange-500)',
+        'color-primary-dark': 'var(--palette-deep-orange-700)',
         'color-accent': 'var(--palette-pink-500)',
+        'color-accent-dark': 'var(--palette-pink-700)',
     }
 };
