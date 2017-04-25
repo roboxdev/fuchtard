a0030f5ad94d2a235f759e00becd68fa3f084b77
robox <roboxv@gmail.com>
2017-04-25 13:29:59 +0600

    animating cart items add/remove

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 
 import { cartOrderingSelector } from 'selectors/app';
 
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+
 import CartItem from 'components/CartItem';
 import CartTotal from 'components/CartTotal';
+import 'styles/Cart.css';
 
+import styles from 'styles/Cart.css';
 
 export class Cart extends React.Component {
     static defaultProps = {
@@ -15,13 +19,28 @@ export class Cart extends React.Component {
     render() {
         const {cart} = this.props;
         return <div>
-            {cart.map((foodItemId) =>
-                <CartItem
-                    key={foodItemId}
-                    foodItemId={+foodItemId}
-                />
-            )}
-            <CartTotal/>
+            <CSSTransitionGroup
+                transitionName={{
+                    enter: styles.slideAnimationEnter,
+                    enterActive: styles.slideAnimationEnterActive,
+                    leave: styles.slideAnimationLeave,
+                    leaveActive: styles.slideAnimationLeaveActive,
+                }}
+                transitionEnterTimeout={1000}
+                transitionLeaveTimeout={1000}>
+                {cart.map((foodItemId) =>
+                    <div
+                        className={styles.cartItemAnimationWrapper}
+                        key={foodItemId}
+                    >
+
+                        <CartItem
+                            foodItemId={+foodItemId}
+                        />
+                    </div>
+                )}
+                <CartTotal/>
+            </CSSTransitionGroup>
         </div>
 
     }
